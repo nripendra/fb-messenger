b30d48ff2a60bdd4bee8ebe847308ba09fe3c717
nripendra <nripendra.newa@gmail.com>
2015-09-19 11:29:45 +0545

    Part 11
    Listing friends

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import ChatStore from '../stores/chatstore';
 import ChatActions from '../actions/chatactions';
+import {Hbox, Vbox} from './layout';
+import AppStores from '../appstores';
+import connectToStore from '../decorators/connectToStores';
 
 export class ChatProps {
     api: any;
     store: ChatStore
 }
 
+@connectToStore(['chatStore'])
 export default class Chat extends React.Component<ChatProps, any> {
     constructor(props: ChatProps) {
         super();
@@ -15,6 +19,27 @@ export default class Chat extends React.Component<ChatProps, any> {
     }
 
     render() {
-        return (<span>Welcome to chat</span>);
+        var state = {
+            'online': {
+                border: '1px solid gray',
+                borderLeft: '3px solid green'
+            },
+            'offline': {
+                border: '1px solid gray',
+                borderLeft: '3px solid gray'
+            }
+        };
+        var friendList = Object.keys(AppStores.chatStore.friendList || []).map(id => AppStores.chatStore.friendList[id]);
+        return (<Hbox>
+                  <Vbox>
+                  {friendList.map(function(friend) {
+                      return (<Hbox>
+                                <img src={friend.thumbSrc} style={state[friend.onlineState || 'offline']} />
+                                {friend.name}
+                          </Hbox>);
+                  }) }
+                      </Vbox>
+                  <Vbox></Vbox>
+            </Hbox>);
     }
 }
