691dd8d83993bb5f4e57ffce722a4bb3ba330a0d
nripendra <nripendra.newa@gmail.com>
2015-11-25 04:29:44 +0545

    Just installing react tsd seems to install react-dom also.

--- tools/typings/tsd.d.ts
+++ tools/typings/tsd.d.ts
@@ -3,7 +3,6 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="jsdom/jsdom.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
 /// <reference path="semver/semver.d.ts" />
 /// <reference path="delorean/delorean.d.ts" />
 /// <reference path="./fbMessengerApp.d.ts" />
