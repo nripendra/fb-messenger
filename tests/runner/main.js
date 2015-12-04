1f89bf032c97b9ae494a9999f6e0a58a08d83a24
nripendra <nripendra.newa@gmail.com>
2015-12-04 13:47:04 +0545

    Make browserwindow invisible on windows. Set exit code on window close.

--- tests/runner/main.js
+++ tests/runner/main.js
@@ -1,34 +1,41 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var ipc = require('electron').ipcMain;
-var path = require('path');
-var renderer_test_exit_status = 0;
+(function () {
+    var app = require('app');
+    var BrowserWindow = require('browser-window');
+    var ipc = require('electron').ipcMain;
+    var path = require('path');
+    var renderer_test_exit_status = 0;
 
-function openElectronWindow() {
-    let w = new BrowserWindow({width: 800, height: 600});
-    let html = 'file://' + path.resolve(__dirname, 'index.html');
-    w.on('closed', function() { process.exit(0) });
-    w.webContents.on('dom-ready', function(){ 
-        w.openDevTools();
-    });
-    w.loadURL(html);
+    function openElectronWindow() {
+        var w = new BrowserWindow({ width: 800, height: 600, x: -800, y: -600, 'title-bar-style': 'hidden', transparent: true, frame: false, skipTaskbar: true });
+        var html = 'file://' + path.resolve(__dirname, 'index.html');
 
-    ipc.on('renderer-test-result', function(event, exit_status){
-        renderer_test_exit_status = exit_status;
-        app.quit();
-    });
-    
-    ipc.on('renderer-print-message', function(event, message){
-        process.send(message);
-    });
-}
+        w.on('closed', function () {
+            process.exit(renderer_test_exit_status)
+        });
 
-app.on('ready', function() {
-    openElectronWindow();
+        w.webContents.on('dom-ready', function () {
+            w.openDevTools();
+        });
 
-    process.on('exit', function() {
-        process.exit(renderer_test_exit_status);
+        ipc.on('renderer-test-result', function (event, exit_status) {
+            renderer_test_exit_status = exit_status;
+            app.quit();
+        });
+
+        ipc.on('renderer-print-message', function (event, message) {
+            process.send(message);
+        });
+
+        w.loadURL(html);
+    }
+
+    app.on('ready', function () {
+        openElectronWindow();
+
+        process.on('exit', function () {
+            process.exit(renderer_test_exit_status);
+        });
     });
-});
+})();
